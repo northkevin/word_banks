b3bf12269888395d75c2cf0722a9987f62aebe6f
northkevin <dev@northkevin.com>
2019-09-29 14:00:22 -0700

    added room channel

--- lib/word_banks_web/channels/user_socket.ex
+++ lib/word_banks_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule WordBanksWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", WordBanksWeb.RoomChannel
+  channel "room:lobby", WordBanksWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
