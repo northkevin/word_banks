11ac43763d7748ca247b453bfb623736eab9bf24
northkevin <dev@northkevin.com>
2019-10-05 13:22:55 -0700

    finished adding decrypt & get_key methods to use application environment variables

--- config/config.exs
+++ config/config.exs
@@ -25,6 +25,42 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# run shell command to "source .env" to load the environment variables.
+# wrap in "try do"
+try do
+  # in case .env file does not exist.
+  File.stream!("./.env")
+  # remove excess whitespace
+  |> Stream.map(&String.trim_trailing/1)
+  # loop through each line
+  |> Enum.each(fn line ->
+    line
+    # remove "export" from line
+    |> String.replace("export ", "")
+    # split on *first* "=" (equals sign)
+    |> String.split("=", parts: 2)
+    # stackoverflow.com/q/33055834/1148249
+    |> Enum.reduce(fn value, key ->
+      # set each environment variable
+      System.put_env(key, value)
+    end)
+  end)
+rescue
+  _ -> IO.puts("no .env file found!")
+end
+
+# Set the Encryption Keys as an "Application Variable" accessible in aes.ex
+config :word_banks, Encryption.AES,
+  # get the ENCRYPTION_KEYS env variable
+  keys:
+    System.get_env("ENCRYPTION_KEYS")
+    # remove single-quotes around key list in .env
+    |> String.replace("'", "")
+    # split the CSV list of keys
+    |> String.split(",")
+    # decode the key.
+    |> Enum.map(fn key -> :base64.decode(key) end)
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
---
+++ lib/encryption/aes.ex
@@ -0,0 +1,73 @@
+defmodule Encryption.AES do
+  # Use AES 256 Bit Keys for Encryption.
+  @aad "AES256GCM"
+
+  def encrypt(plaintext) do
+    # create random Initialisation Vector
+    iv = :crypto.strong_rand_bytes(16)
+    # get the *latest* key in the list of encryption keys
+    key = get_key()
+    {ciphertext, tag} = :crypto.block_encrypt(:aes_gcm, key, iv, {@aad, to_string(plaintext), 16})
+    # "return" iv with the cipher tag & ciphertext
+    iv <> tag <> ciphertext
+  end
+
+  def decrypt(ciphertext) do
+    <<iv::binary-16, tag::binary-16, ciphertext::binary>> = ciphertext
+    :crypto.block_decrypt(:aes_gcm, get_key(), iv, {@aad, ciphertext, tag})
+  end
+
+  defp get_key do
+    keys = Application.get_env(:word_banks, Encryption.AES)[:keys]
+    # get the last/latest key from the key list
+    count = Enum.count(keys) - 1
+    # use get_key/1 to retrieve the desired encryption key.
+    get_key(count)
+  end
+
+  defp get_key(key_id) do
+    # cached call
+    keys = Application.get_env(:word_banks, Encryption.AES)[:keys]
+    # retrieve the desired key by key_id from keys list.
+    Enum.at(keys, key_id)
+  end
+
+  # # this is a "dummy function" we will update it in step 3.3
+  # defp get_key do
+  #   <<
+  #     109,
+  #     182,
+  #     30,
+  #     109,
+  #     203,
+  #     207,
+  #     35,
+  #     144,
+  #     228,
+  #     164,
+  #     106,
+  #     244,
+  #     38,
+  #     242,
+  #     106,
+  #     19,
+  #     58,
+  #     59,
+  #     238,
+  #     69,
+  #     2,
+  #     20,
+  #     34,
+  #     252,
+  #     122,
+  #     232,
+  #     110,
+  #     145,
+  #     54,
+  #     # return a random 32 Byte / 128 bit binary to use as key.
+  #     241,
+  #     65,
+  #     16
+  #   >>
+  # end
+end
