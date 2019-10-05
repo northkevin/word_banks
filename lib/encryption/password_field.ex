3878c74387851c8516e8989024c3c5384625f008
northkevin <dev@northkevin.com>
2019-10-05 13:57:26 -0700

    added verification for stored passwords & tests

--- lib/encryption/password_field.ex
+++ lib/encryption/password_field.ex
@@ -2,4 +2,8 @@ defmodule WordBanks.PasswordField do
   def hash_password(value) do
     Argon2.Base.hash_password(to_string(value), Argon2.gen_salt(), [{:argon2_type, 2}])
   end
+
+  def verify_password(password, stored_hash) do
+    Argon2.verify_pass(password, stored_hash)
+  end
 end
--- test/encryption/password_field_test.exs
+++ test/encryption/password_field_test.exs
@@ -8,4 +8,18 @@ defmodule WordBanks.PasswordFieldTest do
     verified = Argon2.verify_pass(password, hash)
     assert verified
   end
+
+  test "verify_password checks the password against the Argon2id Hash" do
+    password = "EverythingisAwesome"
+    hash = Field.hash_password(password)
+    verified = Field.verify_password(password, hash)
+    assert verified
+  end
+
+  test ".verify_password fails if password does NOT match hash" do
+    password = "EverythingisAwesome"
+    hash = Field.hash_password(password)
+    verified = Field.verify_password("LordBusiness", hash)
+    assert !verified
+  end
 end
